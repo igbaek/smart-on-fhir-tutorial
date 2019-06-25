506480e5921ff2926d997ca6c3d40ffff7038096
In-Geol Baek <ig.baek@e2ehealth.com>
2019-06-25 12:24:45 +0900

    Add log and url parameters print

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -10,6 +10,9 @@
     function onReady(smart)  {
       if (smart.hasOwnProperty('patient')) {
         var patient = smart.patient;
+
+        console.log(patient);
+
         var pt = patient.read();
         var obv = smart.patient.api.fetchAll({
                     type: 'Observation',
